07dd7283b57e9994893835e0a463341adbee8e1b
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-11-13 13:13:30 +0000

    Added MMOEASA operators' boilerplate code

--- MMOEASA.py
+++ MMOEASA.py
@@ -1,3 +1,4 @@
+from MMOEASAOperators import Mutation1, Mutation2, Mutation3, Mutation4, Mutation5, Mutation6, Mutation7, Mutation8, Mutation9, Mutation10, Crossover1
 from problemInstance import ProblemInstance
 from vehicle import Vehicle
 from typing import List
@@ -53,11 +54,30 @@ def Cooling(P: List[Solution], T_stop: float) -> bool:
 
 def Crossover(I: Solution, P_crossover: int):
     if random.randint(1, 100) <= P_crossover:
-        pass
+        Crossover1()
 
 def Mutation(I: Solution, P_mutation: int, probability: int):
     if random.randint(1, 100) <= P_mutation:
-        pass
+        if probability >= 1 and probability <= 10:
+            Mutation1()
+        elif probability >= 11 and probability <= 20:
+            Mutation2()
+        elif probability >= 21 and probability <= 30:
+            Mutation3()
+        elif probability >= 31 and probability <= 40:
+            Mutation4()
+        elif probability >= 41 and probability <= 50:
+            Mutation5()
+        elif probability >= 51 and probability <= 60:
+            Mutation6()
+        elif probability >= 61 and probability <= 70:
+            Mutation7()
+        elif probability >= 71 and probability <= 80:
+            Mutation8()
+        elif probability >= 81 and probability <= 90:
+            Mutation9()
+        elif probability >= 91 and probability <= 100:
+            Mutation10()
 
 def MO_Metropolis(I: Solution, I_m: Solution, T: float):
     pass
@@ -78,10 +98,10 @@ def MMOEASA(instance: ProblemInstance, p: int, MS: int, TC: int, P_crossover: in
                 for j, I in enumerate(P):
                     if j > 0: # I added this because I need to give Crossover and Mutation two parents; the pseudocode says to only give them one but if I do that then I don't have two parents to use in crossover/mutation
                         I_c = Crossover(I, P[j - 1], P_crossover)
-                        I_m = Mutation(I_c, P_mutation, random.randint(1, 10))
+                        I_m = Mutation(I_c, P_mutation, random.randint(1, 100))
                         P[j] = MO_Metropolis(I, I_m, I.T)
                     if True:
                         ND.append(P[j])
-                    P[j].t = P[j].t * P[j].T_cooling
+                    P[j].t *= P[j].T_cooling
             current_multi_start += 1
     return ND
---
+++ MMOEASAOperators.py
@@ -0,0 +1,32 @@
+def Mutation1():
+    pass
+
+def Mutation2():
+    pass
+
+def Mutation3():
+    pass
+
+def Mutation4():
+    pass
+
+def Mutation5():
+    pass
+
+def Mutation6():
+    pass
+
+def Mutation7():
+    pass
+
+def Mutation8():
+    pass
+
+def Mutation9():
+    pass
+
+def Mutation10():
+    pass
+
+def Crossover1():
+    pass
