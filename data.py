26e7f99759592f0a2c83fd027da9fc7c247aa2e3
Taylor Courtney <taylorcourtney2000@googlemail.com>
2021-11-30 15:22:15 +0000

    Cleaned up skipping of empty lines

--- data.py
+++ data.py
@@ -7,19 +7,14 @@ from typing import List
 def loadInstance(filename) -> ProblemInstance:
     try:
         with open(filename) as file:
-            lines = []
             problemInstance = None
             problemName = file.readline().strip() # because the problem name is the first line in the text files, this line quickly adds it to a variable (so we can add it to a "ProblemInstance" object later"
             next(file) # skips the first line (containing the problem name), preventing it from being iterated over
 
             for line in file:
                 if line is not None and not re.search('[a-zA-Z]', line): # checks if the current line contains any characters; we don't want to work with any of the alphabetical characters in the text files, only the numbers
-                    lines.append(line.split())
-
-                    curLine = lines[len(lines) - 1]
-                    if curLine == []: # prevents any work being done with empty lines (lines that contained only a new line; '\n')
-                        del lines[len(lines) - 1]
-                    else:
+                    curLine = line.split()
+                    if curLine: # prevents any work being done with empty lines (lines that contained only a new line; '\n')
                         if len(curLine) == 2: # if the current line only contains two numbers then it's the line that holds only the amount of vehicles and vehicles' capacity, so use them to make a "ProblemInstance"
                             problemInstance = ProblemInstance(problemName, *curLine)
                         else: # if the current line doesn't contain only two values, it will, instead, always contain seven and lines with seven values represent destinations
