ffe25d4f61c75b278f54c1babf0a9b7561e29f39
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-01-06 14:58:06 +0000

    Added a reminder in internal commentary

--- MMOEASA/mmoeasa.py
+++ MMOEASA/mmoeasa.py
@@ -187,7 +187,7 @@ def MMOEASA(instance: ProblemInstance, p: int, MS: int, TC: int, P_crossover: in
                 I_c = Crossover(instance, I, P, P_crossover)
                 I_m = I_c
                 for j in range(0, random.randint(1, MMOEASA_MAX_SIMULTANEOUS_MUTATIONS)):
-                    I_m = Mutation(instance, I_m, P_mutation, random.randint(1, 30))
+                    I_m = Mutation(instance, I_m, P_mutation, random.randint(1, 30)) # TODO: remember to change 30 to 100 once all 10 mutation operators have been implemented
                 P[i] = MO_Metropolis(I, I_m, I.T)
                 
                 if is_nondominated(P[i], ND): # this should be something like "if P[i] is unique and not dominated by all elements in the Non-Dominated set, then add it to ND and sort ND"
