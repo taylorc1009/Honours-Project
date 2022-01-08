7df076bc8bb5ffa8f5bf69bab1e4e3931255a528
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-01-08 13:08:57 +0000

    Adjusted attributes and added __deepcopy__ override

--- vehicle.py
+++ vehicle.py
@@ -1,14 +1,16 @@
+import copy
+
 from destination import Destination
-from typing import List
+from typing import List, Dict
+
+from node import Node
 from problemInstance import ProblemInstance
 
 class Vehicle():
-    route_distance: float=None
-    
-    def __init__(self, number: int, current_capacity: int=0, destinations: List[Destination]=list()) -> None:
-        #self.number: int=number
+    def __init__(self, current_capacity: int=0, destinations: List[Destination]=list(), route_distance: float=0.0) -> None:
         self.current_capacity: int=current_capacity
         self.destinations: List[Destination]=destinations
+        self.route_distance: float=float(route_distance)
 
     def __str__(self) -> str:
         return f"{self.current_capacity}, {[destination.node.number for destination in self.destinations]}"
@@ -56,3 +58,27 @@ class Vehicle():
         for i in enumerate(self.destinations):
             self.destinations[i].assignedVehicle = None
         self.destinations.clear()"""
+
+    def __deepcopy__(self, memodict: Dict=None):
+        obj_copy = Vehicle(
+            current_capacity=self.current_capacity,
+            route_distance=self.route_distance,
+            destinations=[
+                Destination(
+                    node=Node(
+                        number=d.node.number,
+                        x=d.node.x,
+                        y=d.node.y,
+                        demand=d.node.demand,
+                        ready_time=d.node.ready_time,
+                        due_date=d.node.due_date,
+                        service_duration=d.node.service_duration
+                    ),
+                    arrival_time = d.arrival_time,
+                    departure_time = d.departure_time,
+                    wait_time = d.wait_time
+                ) for d in self.destinations
+            ]
+        )
+
+        return obj_copy
