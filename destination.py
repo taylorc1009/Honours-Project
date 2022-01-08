d7c7055b2c472fd2c0ff19f6e6fdd1b2830f8eb3
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-01-08 13:07:58 +0000

    Removed unnecessary "None" node check

--- destination.py
+++ destination.py
@@ -1,16 +1,15 @@
-from typing import List
+import copy
+from typing import List, Dict
 from node import Node
 
 class Destination():
     #assignedVehicle: int=None
 
     def __init__(self, node: Node=None, arrival_time: float=0.0, departure_time: float=0.0, wait_time: float=0.0) -> None:
+        self.node: Node=node
         self.arrival_time: float=arrival_time
         self.departure_time: float=departure_time
         self.wait_time: float=wait_time
-        
-        if not node is None:
-            self.node: Node=node
     
     def __str__(self) -> str:
         return f"{self.arrival_time}, {self.departure_time}, {self.wait_time}, {self.node.__str__()}"
