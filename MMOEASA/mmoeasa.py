be499589d68db5c2c306f1dbe2e70231c1324c84
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-02-01 11:28:32 +0000

    Improved code readability slightly

--- MMOEASA/mmoeasa.py
+++ MMOEASA/mmoeasa.py
@@ -54,7 +54,7 @@ def TWIH(instance: ProblemInstance) -> Solution:
     return solution
 
 def Calculate_cooling(i: int, T_max: float, T_min: float, T_stop: float, p: int, TC: int) -> float:
-    jump_temperatures = (T_max - T_min)/(float(p) - 1.0) if p > 1 else 0.0
+    jump_temperatures = (T_max - T_min) / float(p - 1) if p > 1 else 0.0
     T_2 = T_max - float(i) * jump_temperatures
     error = float(INT_MAX)
     maxError = 0.005 * float(TC)
