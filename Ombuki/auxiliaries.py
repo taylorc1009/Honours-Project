241708976515c9a65863f8c3ab8239b38eb030f2
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-02-02 18:46:04 +0000

    Started implementing population initialiser and changes rand "List" type to "Set"

--- Ombuki/auxiliaries.py
+++ Ombuki/auxiliaries.py
@@ -1,8 +1,8 @@
 from numpy import random
 from constants import INT_MAX
-from typing import List
+from typing import Set
 
-def rand(start: int, end: int, exclude_values: List[int]=None) -> int:
+def rand(start: int, end: int, exclude_values: Set[int]=None) -> int:
     # '+ 1' to make the random number generator inclusive of the "end" value
     offset = 1 if end < INT_MAX else 0
     random_val = random.randint(start, end + offset)
--- Ombuki/ombuki.py
+++ Ombuki/ombuki.py
@@ -1,6 +1,29 @@
 from typing import List
+from numpy import ceil
 from problemInstance import ProblemInstance
 from solution import Solution
+from auxiliaries import rand
+
+def generate_initial_solution(instance: ProblemInstance) -> Solution:
+    num_nodes = len(instance.nodes) - 1
+    num_greedy_nodes = ceil(float(num_nodes * 0.1))
+    num_random_nodes = num_nodes - num_greedy_nodes
+
+    greedy_nodes = set()
+    for _ in range(0, num_greedy_nodes):
+        greedy_nodes.add(instance.nodes[rand(1, num_nodes + 1, exclude_values=greedy_nodes)].number)
+
+    for i in range(1, num_nodes + 2):
+        if i in greedy_nodes:
+            # do greedy procedure
+            pass
+        else:
+            vehicle = rand(1, instance.amount_of_vehicles)
 
 def Ombuki(instance: ProblemInstance, p: int, span: int, crossover: float, mutation: float) -> List[Solution]:
-    pass
+    P: List[Solution] = list()
+    ND: List[Solution] = list()
+
+    initial_solution = generate_initial_solution(instance)
+
+    return ND
