a1a1b3d2bfb849aa50b293689e6e439730c718da
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-02-02 18:23:38 +0000

    Added constants and auxiliary functions

---
+++ Ombuki/auxiliaries.py
@@ -0,0 +1,11 @@
+from numpy import random
+from constants import INT_MAX
+from typing import List
+
+def rand(start: int, end: int, exclude_values: List[int]=None) -> int:
+    # '+ 1' to make the random number generator inclusive of the "end" value
+    offset = 1 if end < INT_MAX else 0
+    random_val = random.randint(start, end + offset)
+    while exclude_values is not None and random_val in exclude_values:
+        random_val = random.randint(start, end + offset)
+    return random_val
