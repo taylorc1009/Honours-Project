ee9f0737ac524e39401e3424ab373692571cb042
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-02-03 17:34:59 +0000

    Revert "Improved help message"
    This reverts commit 3f2de49377bddd9c40f852f34c4a2718d5009d52.

--- Ombuki/ombuki.py
+++ Ombuki/ombuki.py
@@ -1,61 +1,29 @@
-import random
 from typing import List
+from numpy import ceil
 from problemInstance import ProblemInstance
-from Ombuki.solution import Solution
-from vehicle import Vehicle
-from destination import Destination
-from Ombuki.auxiliaries import rand
-from numpy import arange, round
-from Ombuki.constants import INT_MAX
+from solution import Solution
+from auxiliaries import rand
 
-def generate_random_solution(instance: ProblemInstance) -> Solution:
-    solution = Solution(vehicles=list())
+def generate_initial_solution(instance: ProblemInstance) -> Solution:
+    num_nodes = len(instance.nodes) - 1
+    num_greedy_nodes = ceil(float(num_nodes * 0.1))
+    num_random_nodes = num_nodes - num_greedy_nodes
 
-    for i in arange(1, len(instance.nodes) + 1):
-        vehicle = rand(0, instance.amount_of_vehicles - 1) # generate a random number between 1 and max vehicles instead of 1 to current num of vehicles; to keep the original probability of inserting the node to a new vehicle
-        if vehicle < len(solution.vehicles):
-            solution.vehicles[vehicle].destinations.append(Destination(node=instance.nodes[i]))
+    greedy_nodes = set()
+    for _ in range(0, num_greedy_nodes):
+        greedy_nodes.add(instance.nodes[rand(1, num_nodes + 1, exclude_values=greedy_nodes)].number)
+
+    for i in range(1, num_nodes + 2):
+        if i in greedy_nodes:
+            # do greedy procedure
+            pass
         else:
-            solution.vehicles.append(Vehicle(destinations=[Destination(node=instance.nodes[i])]))
+            vehicle = rand(1, instance.amount_of_vehicles)
 
-    return solution
-
-def generate_greedy_solution(instance: ProblemInstance) -> Solution:
-    solution = Solution(vehicles=list().append(Vehicle()))
-    unvisited_nodes = list(arange(1, len(instance.nodes)))
-    vehicle = 0
-
-    while unvisited_nodes:
-        node = random.sample(unvisited_nodes, 1)[0]
-        destination = Destination(node=instance.nodes[node])
-        solution.vehicles[vehicle].destinations.append(destination)
-        unvisited_nodes.remove(node)
-
-        while not solution.vehicles[vehicle].current_capacity > instance.capacity_of_vehicles:
-            closest_node = None
-            distance_of_closest = float(INT_MAX)
-            for node in unvisited_nodes:
-                distance = destination.node.get_distance(instance.nodes[node])
-                if distance < distance_of_closest:
-                    closest_node = node
-                    distance_of_closest = distance
-            if closest_node and not solution.vehicles[vehicle].current_capacity + instance.nodes[closest_node].demand > instance.capacity_of_vehicles:
-                solution.vehicles[vehicle].destinations.append(Destination(node=instance.nodes[closest_node]))
-                solution.vehicles[vehicle].current_capacity += instance.nodes[closest_node].demand
-                unvisited_nodes.remove(destination.node.number)
-            else:
-                break
-
-    return solution
-
-def Ombuki(instance: ProblemInstance, population_size: int, span: int, crossover: float, mutation: float) -> List[Solution]:
+def Ombuki(instance: ProblemInstance, p: int, span: int, crossover: float, mutation: float) -> List[Solution]:
     P: List[Solution] = list()
     ND: List[Solution] = list()
 
-    num_greedy_solutions = round(float(population_size * 0.1))
-    for i in arange(0, num_greedy_solutions):
-        P.insert(i, generate_greedy_solution(instance))
-    for i in arange(num_greedy_solutions, population_size):
-        P.insert(i, generate_random_solution(instance))
+    initial_solution = generate_initial_solution(instance)
 
     return ND
