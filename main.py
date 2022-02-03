6359f423921dba3f237687abe4c57f39b8ffb0d4
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-02-03 17:36:20 +0000

    Improved help message

--- main.py
+++ main.py
@@ -2,20 +2,15 @@ import sys
 import os
 import json
 from problemInstance import ProblemInstance
-from data import openIterationsOfProblemSet
+from data import open_problem_instance
 from MMOEASA.mmoeasa import MMOEASA, TWIH_ref_point
 from MMOEASA.evaluation import calculate_median_Hypervolumes
 from MMOEASA.constants import MMOEASA_POPULATION_SIZE
 from Ombuki.ombuki import Ombuki
 
-def executeMMOEASA(problemInstance: ProblemInstance) -> None:
-    #for i, _ in enumerate(problemInstances):
-        #print(problemInstances[i].name, len(problemInstances[i].MMOEASA_distances))
-    #problemInstances[i].calculateDistances()
+def execute_MMOEASA(problemInstance: ProblemInstance) -> None:
     problemInstance.calculate_distances()
 
-    #for instance in problemInstances:
-    #print(instance.name)
     num_customers = len(problemInstance.nodes) - 1
     with open(f"solomon_{num_customers}/hypervolumes.json") as json_file:
         TC = 0 # termination condition = the number of iterations to perform
@@ -32,7 +27,7 @@ def executeMMOEASA(problemInstance: ProblemInstance) -> None:
             print("\n", str(solution))
         print(calculate_median_Hypervolumes(ND_solutions, TWIH_ref_point(problemInstance))) # TODO: currently, the TWIH usually has a cargo unbalance of 20 and the ND_solutions usually have as low as 90; therefore, TWIH_ref_point's Hypervolume may need to be multiplied by a higher value
 
-def executeOmbuki(problemInstance: ProblemInstance) -> None:
+def execute_Ombuki(problemInstance: ProblemInstance) -> None:
     ND_solutions = Ombuki(problemInstance, 300, 350, 0.8, 0.1)
     for solution in ND_solutions:
         print("\n", str(solution))
@@ -41,34 +36,33 @@ if __name__ == '__main__':
     if not 1 < len(sys.argv) < 4:
         print("If you're unsure how to use the application, give the argument -h (--help) for information")
     elif sys.argv[1] in {"--help", "-h"}: # if the user gave one of these arguments on the command line then a help message is ourputted
-        print(f"There's multiple types of problems in Solomon's instances, here's what they are:{os.linesep}{os.linesep}"
-              f"- Number of customers:"
-              f"  - 25 - 25 customers{os.linesep}"
-              f"  - 50 - 50 customers{os.linesep}"
-              f"  - 100 - 100 customers{os.linesep}{os.linesep}"
-              f"- Customers' location:"
-              f"  - C - clustered customers{os.linesep}"
-              f"  - R - uniformly distributed customers{os.linesep}"
-              f"  - RC - a mix of R and C{os.linesep}{os.linesep}"
-              f"- Width of deliveries' time windows:"
-              f"  - 1 - destinations with narrow time windows{os.linesep}"
-              f"  - 2 - destinations with wide time windows{os.linesep}{os.linesep}"
-              f"To execute a problem set, please enter a set's details. The details required, and in this order, are:{os.linesep}"
-              f"  1. Amount of customers - can be either '25', '50' or '100'{os.linesep}"
-              f"  2. Type of problem - can be either 'C', 'R', or 'RC'{os.linesep}"
-              f"  3. Problem set - can be either '1' or '2'{os.linesep}{os.linesep}"
-              f"An example command is: \"main.py 25 RC 2\""
+        print(f"Format: main(.py) [ algorithm ] [ problem instance ]{os.linesep}{os.linesep}"
+              f"The algorithms available to solve the problem are:{os.linesep}"
+              f" - MMOEASA{os.linesep}"
+              f" - Ombuki{os.linesep}{os.linesep}"
+              f"There's multiple types of problems in Solomon's instances, here's what they are:{os.linesep}"
+              f" - Number of customers:{os.linesep}"
+              f"   - 25 - 25 customers{os.linesep}"
+              f"   - 50 - 50 customers{os.linesep}"
+              f"   - 100 - 100 customers{os.linesep}"
+              f" - Customers' location:{os.linesep}"
+              f"   - C - clustered customers{os.linesep}"
+              f"   - R - uniformly distributed customers{os.linesep}"
+              f"   - RC - a mix of R and C{os.linesep}"
+              f" - Width of deliveries' time windows:{os.linesep}"
+              f"   - 1 - destinations with narrow time windows{os.linesep}"
+              f"   - 2 - destinations with wide time windows{os.linesep}{os.linesep}"
+              f"To execute a problem set, please enter a problem's filename. The details required, and the argument format, are:{os.linesep}"
+              f" - solomon_[ number of customers ]/[ customers' location ][ width of time windows ]XX.txt (where XX is the instance number; see the folder \"solomon_[ number of customers ]\" for available instances){os.linesep}{os.linesep}"
+              f"An example command is: \"main.py MMOEASA solomon_100/C101.txt\""
         )
     elif len(sys.argv) == 3:
-        problemInstances = openIterationsOfProblemSet(sys.argv[2])#*sys.argv[1:])
-
-        #if len(problemInstances) > 0:
-            #print([problemInstance.__str__() for problemInstance in problemInstances])
+        problemInstance = open_problem_instance(sys.argv[2])
 
         if sys.argv[1].upper() == "MMOEASA":
-            executeMMOEASA(problemInstances)
+            execute_MMOEASA(problemInstance)
         elif sys.argv[1].upper() == "OMBUKI":
-            executeOmbuki(problemInstances)
+            execute_Ombuki(problemInstance)
         else:
             exc = ValueError(f"Algorithm \"{sys.argv[1]}\" was not recognised")
             raise exc
