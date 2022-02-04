a7a0478f51ea9762aace91d194e7c31888dc884f
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-02-04 17:30:49 +0000

    Fixed Destination's attribute typing

--- destination.py
+++ destination.py
@@ -5,9 +5,9 @@ from node import Node
 class Destination:
     def __init__(self, node: Node=None, arrival_time: float=0.0, departure_time: float=0.0, wait_time: float=0.0) -> None:
         self.node: Node=node
-        self.arrival_time: float=arrival_time
-        self.departure_time: float=departure_time
-        self.wait_time: float=wait_time
+        self.arrival_time: float=float(arrival_time)
+        self.departure_time: float=float(departure_time)
+        self.wait_time: float=float(wait_time)
     
     def __str__(self) -> str:
         return f"Destination(arrival_time={self.arrival_time}, departure_time={self.departure_time}, wait_time={self.wait_time}, {str(self.node)})"
