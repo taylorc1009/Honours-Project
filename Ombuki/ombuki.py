2933d4d3c6aa0d6354f1c6a13fd1c06051e41970
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-02-07 15:06:23 +0000

    Fixed syntax

--- Ombuki/ombuki.py
+++ Ombuki/ombuki.py
@@ -64,7 +64,7 @@ def generate_greedy_solution(instance: ProblemInstance) -> Solution:
 def is_nondominated(old_solution: Solution, new_solution: Solution) -> bool:
     return (new_solution.total_distance < old_solution.total_distance and len(new_solution.vehicles) <= len(old_solution.vehicles)) or (new_solution.total_distance <= old_solution.total_distance and len(new_solution.vehicles) < len(old_solution.vehicles))
 
-def pareto_rank(population: List[Solution], ) -> None:
+def pareto_rank(population: List[Solution]) -> None:
     curr_rank = 1
     N = len(population)
     unranked_solutions = set(arange(0, N))
