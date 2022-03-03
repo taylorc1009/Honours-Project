e4f0ef37419d702a3fb021dde40437e4fab268c8
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-03-03 09:51:38 +0000

    Updated internal commentary

--- MMOEASA/parameters.py
+++ MMOEASA/parameters.py
@@ -2,7 +2,7 @@ from typing import Final
 
 POPULATION_SIZE: Final[int]=20
 MULTI_STARTS: Final[int]=10
-TERMINATION_CONDITION_ITERATIONS: Final[int]=200
+TERMINATION_CONDITION_ITERATIONS: Final[int]=200 # if you use "iterations" as the termination condition, then the number of iterations that will be performed will be equal to TERMINATION_CONDITION_ITERATIONS * MULTI_STARTS
 TERMINATION_CONDITION_SECONDS: Final[int]=600
 TERMINATION_CONDITION_TYPE: Final[str]="seconds" # can also be set to "iterations", thus using the TERMINATION_CONDITION_ITERATIONS parameter instead of TERMINATION_CONDITION_SECONDS
 CROSSOVER_PROBABILITY: Final[int]=25
