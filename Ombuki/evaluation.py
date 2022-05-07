7408d647e59a01391630c8d296beb06da83d3cb6
Taylor Courtney <taylorcourtney2000@googlemail.com>
2022-05-07 13:24:21 +0100

    Slightly improved Ombuki's objective function evaluation

--- Ombuki/evaluation.py
+++ Ombuki/evaluation.py
@@ -6,7 +6,7 @@ def ref_point(instance: ProblemInstance) -> Tuple[float, float]:
     return 10000.0, instance.amount_of_vehicles
 
 def calculate_Hypervolumes_area(nondominated_set: List[OmbukiSolution], ref_Hypervolumes: Tuple[float, float]) -> float:
-    prev_TD, prev_NV = ref_TD, ref_NV = ref_Hypervolumes[0], ref_Hypervolumes[0]
+    prev_TD, prev_NV = ref_TD, ref_NV = ref_Hypervolumes[0], ref_Hypervolumes[1]
     area = 0.0
 
     for solution in sorted([s for s in nondominated_set], key=lambda x: x.total_distance, reverse=True):
